2796e51aaea9f5413c51fff01ac3c49c4374b5c8
arunaaaa66 <kangreact2@gmail.com>
2024-10-16 01:31:06 +0800

    added

--- Main.java
+++ Main.java
@@ -1,20 +1,19 @@
 public class Main {
      public static void main(String[] args) {
        Player player = new Player();
-       //player.GetInfoPlayer();
-       //player.moveRight();
-       //player.moveLeft();
-       //player.moveUp();
-       //player.moveDown();
-       //player.getDamage(2, 1);
-       //player.getPower();
-       //player.attack();
-       //player.dash(5);
-       //player.isDead();
-       //player.isRespawn();
+       player.GetInfoPlayer();
+       player.moveRight();
+       player.moveLeft();
+       player.moveUp();
+       player.moveDown();
+       player.getDamage(2, 1);
+       player.getPower();
+       player.attack();
+       player.dash(5);
+       player.isDead();
+       player.isRespawn();
        player.firstSkill(true);
        player.secondSkill(true);
        player.ultimateSkill(true);
     }
-    
 }
